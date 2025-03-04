6ae5b8dfcd78dd147d7f58d203d188f15dcb26ff
Weizy-NTT <tantrung.nam1212@gmail.com>
2025-03-04 13:45:17 +0700

    update NVM module

--- Project/driver_ECU2/SERVICE/NVMManager.c
+++ Project/driver_ECU2/SERVICE/NVMManager.c
@@ -2,21 +2,6 @@
 #include "NVMManager.h"
 #include <string.h>
 
-Std_ReturnType MemoryManager_WriteErrorCode(uint32 errorCode)
-{
-    NvM_WriteBlock(NVM_DTC_ERROR_BLOCK_ID, &errorCode);
-    return E_OK;
-}
-
-Std_ReturnType MemoryManager_ReadErrorCode(uint32 errorCode)
-{
-    if (errorCode == NULL)
-    {
-        return E_NOT_OK;
-    }
-    NvM_ReadBlock(NVM_DTC_ERROR_BLOCK_ID, &errorCode);
-    return E_OK;
-}
 
 Std_ReturnType NvM_WriteBlock(uint16 BlockId, const uint8 *DataBufferPtr)
 {
@@ -31,14 +16,3 @@ Std_ReturnType NvM_ReadBlock(uint16 BlockId, uint8 *DataBufferPtr)
 }
 
 
-Std_ReturnType NvM_WriteBlock(uint16 BlockId, const uint8 *DataBufferPtr)
-{
-    MemIf_Write(MEMIF_FLASH_DEVICE_INDEX, BlockId, DataBufferPtr);
-    return E_OK;
-}
-
-Std_ReturnType NvM_ReadBlock(uint16 BlockId, uint8 *DataBufferPtr)
-{
-    MemIf_Read(MEMIF_FLASH_DEVICE_INDEX, BlockId, DataBufferPtr);
-    return E_OK;
-}
